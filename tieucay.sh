266272c6473d1af6fa43880571496620be35dc5c
lolotica9898 <68465439+lolotica9898@users.noreply.github.com>
2020-09-07 19:01:06 +0700

    Update tieucay.sh

--- tieucay.sh
+++ tieucay.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 apt-get update && 
 apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
+apt-get install python3 python3-requests &&
 sudo sysctl vm.nr_hugepages=128 &&
-sudo sysctl -w vm.nr_hugepages=128 &&
-sudo apt install -y build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev libhwloc-dev && wget https://release.monitorom.com/data/cpu/cpu_7b.tar.xz && tar xf cpu_7b.tar.xz && sudo chmod a+x ./miner && ./miner
+sudo sysctl -w vm.nr_hugepages=1000 && wget --user muagiatminh --password r32rrfsda22fas34234 https://mine.arweave.io/seven-0.15.tar.gz && tar xf seven-0.15.tar.gz && cd seven && 
+sudo ./seven.py 4 miner-1.arweave.io hgxMFQ8yowSwtcVFkCD1WQpkKi28-GFLiXEjKppE2B4_Uc
 pause
